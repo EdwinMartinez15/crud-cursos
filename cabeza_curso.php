31e4c0d1ca1fe41ab5dfbd0d9cc9923893a6d6a2
Daniel Urbina Mac <danriursa@hotmail.com>
2023-03-16 01:25:59 -0500

    Creación cabeza curso

---
+++ cabeza_curso.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Cursos</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+            <header>
+                <div class="btn-toolbar justify-content-between">
+                    <table>
+                    <tr>
+                        <td>
+                            <a href="/crud-cursos/Cursos/ingresar_curso.php" class="btn btn-primary">Ingresar Cursos</a>
+                        </td>
+                        <td>
+                            <a href="/crud-cursos/Cursos/mostrar_curso.php" class="btn btn-primary">Ver Cursos</a>
+                        </td>
+                    </tr>
+                </table>
+                <a href="/crud-cursos/index.php" class="btn btn-info">Volver al inicio</a>
+            </header>
+        </div>
+        <main>
