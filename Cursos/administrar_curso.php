6cb32c20ff587e277c0001a670021c4707dbd007
Daniel Urbina Mac <danriursa@hotmail.com>
2023-03-16 01:28:26 -0500

    Creacion administrar curso

---
+++ Cursos/administrar_curso.php
@@ -0,0 +1,27 @@
+<?php
+    require_once('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/Cursos/crud_curso.php');
+    require_once('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/Cursos/curso.php');
+
+
+    $crud = new CrudCurso();
+    $curso = new Curso();
+
+    if (isset($_POST['insertar'])) {
+        $curso->setNombre($_POST['nombre']);
+        $curso->setCodigo($_POST['codigo']);
+        $crud->insertar($curso);
+        header('Location: /crud-cursos/Cursos/mostrar_curso.php');
+    }elseif (isset($_POST['actualizar'])) {
+        $curso->setId($_POST['id']);
+        $curso->setNombre($_POST['nombre']);
+        $curso->setCodigo($_POST['codigo']);
+        $crud->actualizar($curso);
+        header('Location: /crud-cursos/Cursos/mostrar_curso.php');
+    }elseif ($_GET['accion']=='e') {
+        $crud->eliminar($_GET['id']);
+        header('Location: /crud-cursos/Cursos/mostrar_curso.php');
+    }elseif ($_GET['accion']=='a') {
+        header('Location: /crud-cursos/Cursos/actualizar_curso.php');
+    }
+
+?>
