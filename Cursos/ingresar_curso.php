190e00893f0fc96ba4d37d7615ae7542fdf641c1
Daniel Urbina Mac <danriursa@hotmail.com>
2023-03-16 01:29:39 -0500

    Creación ingresar curso

---
+++ Cursos/ingresar_curso.php
@@ -0,0 +1,36 @@
+<?php
+    require_once('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/Cursos/crud_curso.php');
+    require_once('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/Cursos/curso.php');
+    $crudC=new CrudCurso();
+    $curso=new Curso();
+    $listaCursos=$crudC->mostrar();
+    include('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/cabeza_curso.php');
+?>
+<div class="container">
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Ingrese los datos del curso
+                </div>
+                <div class="card-body">
+                    <form action="/crud-cursos/Cursos/administrar_curso.php" method="post">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Nombre</label>
+                                <input  type="text"class="form-control" name="nombre" >
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Codigo</label>
+                                <input type="text"class="form-control" name="codigo">
+                        </div>
+                        <input type="hidden" name="insertar" value="insertar">
+                        <input type="submit" value="Guardar" class="btn btn-success">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <?php
+    include('/Applications/XAMPP/xamppfiles/htdocs/crud-cursos/cola.php')
+    ?>
