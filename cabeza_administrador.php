ba66113e98f0d91e14303ec75548e084e1eab6dc
Daniel Urbina Mac <danriursa@hotmail.com>
2023-03-16 20:56:18 -0500

    Creacion de la cabeza administrador

---
+++ cabeza_administrador.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Usuarios</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+            <header>
+                <div class="btn-toolbar justify-content-between">
+                    <table>
+                        <tr>
+                            <td>
+                                <a href="/crud-cursos/Usuarios/mostrar_usuario.php" class="btn btn-primary">Ver usuarios inscritos</a>
+                            </td>
+                            <td>
+                                <a href="/crud-cursos/Gestores/mostrar_gestor.php" class="btn btn-primary">Ver Gestores inscritos </a>
+                            </td>
+                            <td>
+                                <a href="/crud-cursos/Cursos/mostrar_curso.php" class="btn btn-primary">Ver Cursos inscritos  </a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="position-absolute top-0 end-0">
+                    <table>
+                        <td>
+                            <a href="/crud-cursos/Templates/PantallaAdministrador.php" class="btn btn-info">Volver al inicio</a>
+                        </td>
+                        <td>
+                            <a href="/crud-cursos/Login/login.php" class="btn btn-danger">Cerrar sesión</a>
+                        </td>
+                    </table>
+                </div>
+            </header>
+        <main>
