7da84ccc01a071b21d65e9388ace8e93075ce156
Edwin <edwinmartinez891226@gmail.com>
2023-05-20 11:05:58 -0500

    Toma

--- Login/control_login.php
+++ Login/control_login.php
@@ -1,44 +1,42 @@
 <?php
-    session_start();
-    
-    require_once('../Login/login.php');
-    
-    require_once('../Gestores/crud_gestor.php');
-    require_once('../Gestores/gestor.php');
-    
-    $crudG = new CrudGestor();
-    $gestor = new Gestor();
-    $usuarioG=$_POST['usuario'];
-    $gestor=$crudG->seleccionGC($usuarioG);
-    
-    $_SESSION['Tipo']=$gestor['tipo_usuario'];
-    $_SESSION['Id']=$gestor['id'];
-    $_SESSION['Nombre']=$gestor['nombre'];
+   session_start();
 
+   require_once('../Login/login.php');
+   
+   require_once('../Gestores/crud_gestor.php');
+   require_once('../Gestores/gestor.php');
+   
+     if (isset($_POST['iniciar'])) {
+    $usuario = $_POST['usuario'];
+    $clave = $_POST['clave'];
+    $gestor = $crudG->seleccionGC($usuario);
+
+    $_SESSION['Tipo'] = $gestor['tipo_usuario'];
+    $_SESSION['Id'] = $gestor['id'];
+    $_SESSION['Nombre'] = $gestor['nombre'];
+
+    if ($gestor && $gestor['clave'] == $clave) {
     
-
-
-    if (isset($_POST['iniciar'])) {
-        if ($gestor['clave']==$_POST['clave']) {
-            switch ($gestor['tipo_usuario']) {
-                case 'administrador':
-                    header('Location: /crud-cursos/Templates/Pantallas/PantallaAdministrador.php');
-                    break;
-                case 'gestor':
-                    header('Location: /crud-cursos/Templates/Pantallas/PantallaGestor.php');
-                    break;
-                
-                default:
-                    header('Location: /crud-cursos/Login/login.php');
-                    break;
-            }
+        switch ($gestor['tipo_usuario']) {
+            case 'administrador':
+                header('Location: /crud-cursos/Templates/Pantallas/PantallaAdministrador.php');
+                exit();
+            case 'mesero':
+                header('Location: /crud-cursos/Templates/Pantallas/PantallaMesero.php');
+                exit();
+            case 'cajero':
+                header('Location: /crud-cursos/Templates/Pantallas/PantallaCajero.php');
+                exit();
+            default:
+                header('Location: /crud-cursos/index.php');
+                exit();
         }
-        else {
-            
-        }
+    } else {
+     
+        header('Location: /crud-cursos/index.php?error=1');
+        exit();
     }
-    else {
-    }
-
+}
+   
 
 ?>
